4fb7cc634956ce1afa9b5ec18af5400bd9315db7
moisemomondiaye <momond3@gmail.com>
2022-03-21 15:31:23 +0000

    Lesson 4: Lists and animations

--- app/src/main/java/com/example/jetpackcomposetuto/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetuto/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,9 +30,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTutoTheme {
-                MessageCard(Message("Momzo", "hey it's only a message"))
+                Conversation(messages = SampleData.conversationSample)
             }
         }
+    }
+}
+
+
+@Composable
+fun Conversation(messages : List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewConversation() {
+    JetpackComposeTutoTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
 
@@ -42,16 +66,26 @@ fun MessageCard(message: Message) {
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
+        var isExpanded by remember { mutableStateOf(false)}
+
+        val surfaceColor : Color by  animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(text = message.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2)
 
             Spacer(modifier = Modifier.width(8.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp,
+            color = surfaceColor, modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)) {
                 Text(text = message.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2 )
             }
         }
